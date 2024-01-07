920eb1e5b1cc53f7da9730f354ac310b48c0c6a3
JW <jacekwoln@gmail.com>
2024-01-07 15:30:35 +0100

    a

--- src/index.js
+++ src/index.js
@@ -1,58 +1,59 @@
-const core = require('@actions/core');
-const FormData = require('form-data');
-const fs = require('fs');
-const github = require('@actions/github');
-const cheerio = require('cheerio');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const core = require('@actions/core')
+const FormData = require('form-data')
+const fs = require('fs')
+const github = require('@actions/github')
+const cheerio = require('cheerio')
+const custom_fetch = async (...args) => {
+  const { default: fetch } = await import('node-fetch')
+  return fetch(...args)
+}
 
 async function makeFiles() {
-    try {
-
-        let input = core.getInput('path');
-        if (input === undefined || input === '')
-        {
-            core.setFailed("Input path is not defined!");
-            input = "resources/backpack12.zip"
-        }
-        const formData = new FormData();
-        formData.append('file', fs.createReadStream(input));
-
-
-
-        let response = await fetch("https://mc-packs.net/", {method: 'POST', body: formData});
-        let content = await response.text();
-
-        const $ = cheerio.load(content);
-        const textArea = $("textarea")[0];
-        const textAreaContent =  $(textArea).text()
-
-        let values= extractValues(textAreaContent);
-        let url = values.resourcePack;
-        let hashCode = values.resourcePackSha1;
-
-        core.setOutput("url", url);
-        core.setOutput("hash", hashCode);
-        console.log("result",values);
-
-    } catch (error) {
-        console.log(error)
-        core.setFailed(error.message);
+  try {
+    let input = core.getInput('path')
+    if (input === undefined || input === '') {
+      core.setFailed('Input path is not defined!')
+      input = 'resources/backpack12.zip'
     }
+    const formData = new FormData()
+    formData.append('file', fs.createReadStream(input))
+
+    const response = await custom_fetch('https://mc-packs.net/', {
+      method: 'POST',
+      body: formData
+    })
+    const content = await response.text()
+
+    const $ = cheerio.load(content)
+    const textArea = $('textarea')[0]
+    const textAreaContent = $(textArea).text()
+
+    const values = extractValues(textAreaContent)
+    const url = values.resourcePack
+    const hashCode = values.resourcePackSha1
+
+    core.setOutput('url', url)
+    core.setOutput('hash', hashCode)
+    console.log('result', values)
+  } catch (error) {
+    console.log(error)
+    core.setFailed(error.message)
+  }
 }
 
 function extractValues(inputString) {
-    const resourcePackRegex = /resource-pack=(.+)\s/;
-    const resourcePackSha1Regex = /resource-pack-sha1=(\w+)/;
+  const resourcePackRegex = /resource-pack=(.+)\s/
+  const resourcePackSha1Regex = /resource-pack-sha1=(\w+)/
 
-    const resourcePackMatch = inputString.match(resourcePackRegex);
-    const resourcePackSha1Match = inputString.match(resourcePackSha1Regex);
+  const resourcePackMatch = inputString.match(resourcePackRegex)
+  const resourcePackSha1Match = inputString.match(resourcePackSha1Regex)
 
-    const resourcePack = resourcePackMatch ? resourcePackMatch[1] : null;
-    const resourcePackSha1 = resourcePackSha1Match ? resourcePackSha1Match[1] : null;
+  const resourcePack = resourcePackMatch ? resourcePackMatch[1] : null
+  const resourcePackSha1 = resourcePackSha1Match
+    ? resourcePackSha1Match[1]
+    : null
 
-    return { resourcePack, resourcePackSha1 };
+  return { resourcePack, resourcePackSha1 }
 }
 
-
-
-makeFiles();
+makeFiles()
